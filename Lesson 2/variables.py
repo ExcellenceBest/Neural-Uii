1a009be7dd1b89cbf4e319756c0ac3ee52906800
Excellence <352481@mail.ru>
2024-10-28 13:47:43 +0300

    выполнены первые два задания

---
+++ Lesson 2/variables.py
@@ -0,0 +1,12 @@
+"""
+Объект Авто
+"""
+
+name = 'Hyundai'
+mark = 'ix 35'
+color = 'white'
+year_of_manufacture = 2020
+acceleration = 8.6
+transmission_auto = False
+print(type(name), type(mark), type(color))
+print(type(year_of_manufacture), type(acceleration), type(transmission_auto))

12351cdc14d72fc374972e8964edf6db169133fa
Excellence <352481@mail.ru>
2024-10-28 14:04:32 +0300

    выполнено четвертое задание

---
+++ Lesson 2/borndayforewer.py.py
@@ -0,0 +1,7 @@
+year = None
+day = None
+while year != 1799:
+    year = int(input('Введите год рождения А.С. Пушкина:  '))
+while day != 6:
+    day = int(input('Введите день рождения А.С. Пушкина:  '))
+print('Верно')
---
+++ Lesson 2/bornyearforewer.py
@@ -0,0 +1,4 @@
+year = None
+while year != 1799:
+    year = int(input('Введите год рождения А.С. Пушкина:  '))
+print('Верно')

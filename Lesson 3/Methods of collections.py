ce953723faa381bf0c5875fe5e9c2adad0fa214a
Excellence <352481@mail.ru>
2024-10-31 12:35:12 +0300

    Задания готовы к проверке

---
+++ Lesson 3/Methods of collections.py
@@ -0,0 +1,17 @@
+"""
+List списки.
+
+list.append(x), list.pop([i]), list.copy, list.reverse, list.insert(i,x), list.count(x)/
+
+Dict словари.
+
+dict.get(key), dict.items(), dict.keys(), dict.values(), dict.pop(key)
+
+set множество.
+
+set.add(x), set.pop(), set.copy(), set.onion(), len(x)
+
+str  строка.
+
+string.find(str), string.rfind(str), slice - str[start:stop:step], len(str), str.split(любой символ), string.join(список)
+"""

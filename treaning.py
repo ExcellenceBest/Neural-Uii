5e6de5efc13f10e96648107e3867acc75fe076ea
Peps@ <352481@mail.ru>
2024-11-07 21:20:47 +0300

    Задание доработано

---
+++ treaning.py
@@ -0,0 +1,15 @@
+import numpy as np
+import random
+
+data = [1, 2, 3]
+data2 = [4, 5, 6]
+data3 = [7, 8, 9]
+arr = np.array(data)
+arr2 = np.array(data2)
+arr3 = np.array(data3)
+arr5 = np.ones((2, 3))
+print(arr)
+print(arr5)
+answer = arr + arr5 + arr2
+print(answer)
+a = np.conca
